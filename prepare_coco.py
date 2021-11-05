95bda6f5b9d5cfc815eff014397c4cb1903c5ef5
HM Server <huypr09xyz@gmail.com>
2021-11-05 14:09:58 +0000

    Add some details

--- prepare_coco.py
+++ prepare_coco.py
@@ -3,3 +3,7 @@ import torch
 import cv2
 import numpy as np
 
+#* Assume the data is in coco format
+def get_data(imgFol, jsonPath, datasetName):
+    return register_coco_instances(datasetName, {}, jsonPath, imgFol)
+
